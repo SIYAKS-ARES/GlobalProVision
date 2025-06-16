0b4301cf2aeb920e3cf4083fa71181a56458b264
sudobatu <acarbatuhan.42@gmail.com>
2025-06-16 14:01:47 +0300

    Initial frontend design

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ObjectDetectorUI from './ObjectDetectorUI';
+
+function App() {
+  return (
+    <div className="App">
+      <ObjectDetectorUI />
+    </div>
+  );
+}
+
+export default App;
