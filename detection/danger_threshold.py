ff5f619aa689180088a32b1e44d9812d5418e3ee
Beyza Nur Büyükdemir <155733905+beyzayildirim81@users.noreply.github.com>
2025-06-16 10:48:56 +0300

    changes gitignore

--- detection/danger_threshold.py
+++ detection/danger_threshold.py
@@ -1,22 +1,20 @@
 # Tespit edilecek tehlikeli nesnelerin listesi
+#'knife', 'gun', 'scissors', 'firearm', 'axe', 'sword'
 TEHLIKELI_NESNELER = ['knife', 'gun', 'scissors']
 
-# Modelin tanıyabildiği hayvan sınıflarının listesi
-ANIMAL_CLASSES = [
-    'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant',
-    'bear', 'zebra', 'giraffe'
-]
-
 # Algılama için varsayılan güven eşiği
-DEFAULT_CONF_THRESHOLD = 0.25
+# Bu eşik, 'TEHLIKELI_NESNELER' listesinde olmayan diğer tüm nesneler için geçerli.
+DEFAULT_CONF_THRESHOLD = 0.30
 
 # 'knife' sınıfı için özel güven eşiği
 KNIFE_CONF_THRESHOLD = 0.07
 
 # Tehlike oranı hesaplaması için kurallar:
+# Her tespit edilen insan için eklenecek tehlike puanı
 PERSON_DANGER_SCORE = 5
-DANGEROUS_OBJECT_SCORE = 50
-MAX_DANGER_PERCENTAGE = 100
 
-# Sadece gösterilmesine izin verilen sınıflar (insan, tehlikeli nesneler, hayvanlar)
-# Bu liste app.py içinde dinamik olarak oluşturulacak
+# Tehlikeli bir nesne tespit edildiğinde eklenecek tehlike puanı
+DANGEROUS_OBJECT_SCORE = 50
+
+# Maksimum tehlike oranı yüzdesi
+MAX_DANGER_PERCENTAGE = 100
